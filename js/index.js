0d8a9da68fbad4080bd11d9efdf71f4664a54896
Jerry Hampton <hamptonjt@hotmail.com>
2015-11-25 04:59:03 -0500

    Restructuring page to put button at the bottom of the page

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
-$(document ).ready(function() {
+(function() {
+  $(document ).ready(function() {
+      getVerse();
+  });
+  
+  $("button").click(function(){
     getVerse();
-});
-
-$("button").click(function(){
-  getVerse();
-});
-
-var getVerse = function() {
-  $("#spinner").show();
-    $.ajax({
-      url: "http://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
-      crossDomain: true,
-      dataType: 'jsonp',
-      success: function(result){
-         $("#newQuote")
-           .html('<strong>'+
-                 result[0].bookname+
-                 ' ' + result[0].chapter +
-                 ':' + result[0].verse +
-                 '</strong> ' +
-                 result[0].text);
-        $("#spinner").hide();
-      }
-    });
-}
+  });
+  
+  var getVerse = function() {
+    $("#spinner").show();
+      $.ajax({
+        url: "http://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result){
+          $("#newQuote")
+            .html('<strong>'+
+                  result[0].bookname+
+                  ' ' + result[0].chapter +
+                  ':' + result[0].verse +
+                  '</strong> ' +
+                  result[0].text);
+          $("#spinner").hide();
+        }
+      });
+  }  
+})();
